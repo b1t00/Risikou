7e13e7ea2940ba99c1a1c997cae16d72d029ba9f
AnnCharBaer <48607023+AnnCharBaer@users.noreply.github.com>
2019-04-01 14:35:15 +0200

    Update Weltverwaltung.java

--- src/ris/local/domain/Weltverwaltung.java
+++ src/ris/local/domain/Weltverwaltung.java
@@ -3,7 +3,7 @@ package ris.local.domain;
 import ris.local.valueobjects.Land;
 
 public class Weltverwaltung {
-	Land[] laenderArray = {new Land("Frankreich", 1), new Land("Italien", 2), new Land};
+	Land[] laenderArray = {new Land("Frankreich", 1), new Land("Italien", 2), new Land("Spanien", 3)};
 	
 
 }
