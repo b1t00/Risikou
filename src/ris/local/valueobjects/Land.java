c2021b201951e47baa13b8e9bc19db0e0fc58e2d
AnnCharBaer <48607023+AnnCharBaer@users.noreply.github.com>
2019-04-01 14:15:24 +0200

    Update Land.java

--- src/ris/local/valueobjects/Land.java
+++ src/ris/local/valueobjects/Land.java
@@ -1,5 +1,22 @@
 package ris.local.valueobjects;
 
-public class Land {
 
+
+public class Land {
+	private int einheiten;
+	private int nummer;
+	private String name;
+	
+	private Land(int nummer, String name) {
+		this.nummer = nummer;
+		this.name = name;
+	}
+	
+	public int getNummer() {
+		return nummer;
+	}
+	
+	public int getEinheiten() {
+		return einheiten;
+	}
 }
