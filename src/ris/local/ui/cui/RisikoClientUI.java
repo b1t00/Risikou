31883721fc6b9ed11ebc3f6bde10bf07894268da
HannesLE <hannesl0123@gmail.com>
2019-04-01 16:33:03 +0200

    Update RisikoClientUI.java

--- src/ris/local/ui/cui/RisikoClientUI.java
+++ src/ris/local/ui/cui/RisikoClientUI.java
@@ -2,8 +2,12 @@ package ris.local.ui.cui;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
+import ris.local.valueobjects.Gamer;
+import ris.local.valueobjects.Land;
 public class RisikoClientUI {
+	ArrayList<Land> inBesitz= new ArrayList<Land>();
 	private BufferedReader in;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -21,15 +25,15 @@ public class RisikoClientUI {
 	
 	public void legSpielerAn() {
 		System.out.println("Name von spieler 1: ");
-		Gamer gamer1 = new Gamer(liesEingabe());
+		Gamer gamer1 = new Gamer(liesEingabe(),inBesitz);
 		System.out.println("Name von spieler 2: ");
+		Gamer gamer2 = new Gamer(liesEingabe(),inBesitz);
 	}
 	
 	private String liesEingabe() {
-		return in.readLine();
+			return in.readLine();
 	}
 	
 }
 	
-}
-,
+
