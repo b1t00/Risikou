619d5da2aa342daa9a0f179ddc407bb72bb03953
AnnCharBaer <48607023+AnnCharBaer@users.noreply.github.com>
2019-04-03 14:31:35 +0200

    Update RisikoClientUI.java

---
+++ src/ris/local/domain/Spiellogik.java
@@ -0,0 +1,25 @@
+package ris.local.domain;
+
+import java.util.ArrayList;
+
+import ris.local.valueobjects.Gamer;
+import ris.local.valueobjects.Land;
+
+public class Spiellogik {
+
+	
+	public String angriff(int land, Gamer spieler){
+		ArrayList<Land> feinde = new ArrayList<Land>();
+		for (int i = 0; i < nachbarn[land].length; i++) {
+			if (nachbarn[land][i] && !(laender[land].getFarbe().equals(spieler.getFarbe()))) {
+				feinde.add(laender[i]);
+			}
+		}
+		String result = "";
+		for (Land feind: feinde) {
+			result += feind.getNummer() + " > " + feind.getName() + "\n";
+		}
+		return result;
+	}
+	
+}
