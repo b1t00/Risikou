9438c60fb8181ba6b8f945b60ef6086a65f51d8b
HannesLE <hannesl0123@gmail.com>
2019-04-03 09:13:15 +0200

    Update dice.java

--- src/ris/local/domain/dice.java
+++ src/ris/local/domain/dice.java
@@ -1,51 +1,96 @@
 package ris.local.domain;
+
 import java.util.ArrayList;
 import java.util.Collections;
-public class dice {
-public static void main(String[]args) {
-	dice dice = new dice();
-	dice.rollDice(2, 1);
 
-}
+public class Dice {
 
-	public void rollDice(int defUnits, int attUnits) {
-	ArrayList<Integer> attDice= new ArrayList<Integer>();	
-	ArrayList<Integer> defDice= new ArrayList<Integer>();		
-	// 1 Angreifer, 1 Verteidiger//
-	if(attUnits==1) {
-		if(defUnits==1) {
-			int def1 = (int)(Math.random() *6)+1;
-			int att1 =  (int)(Math.random() *6)+1;
-			System.out.println(att1);
-			System.out.println(def1);
-			if(def1>att1) {
-				System.out.println("def wins");
-			}
-			else
-				System.out.println("att wins");
-		}
-		if(defUnits==2) {
-			int def1=(int)(Math.random() *6)+1;
-			int def2=(int)(Math.random() *6)+1;
-			int att1=(int)(Math.random() *6)+1;
-			System.out.println(def1);
-			System.out.println(def2);
-			System.out.println(att1);
-			if(def1>att1 || def2>att1) {
-				System.out.println("def wins");
-			}
-			else
-				System.out.println("att wins");
-		}
-		
-		
-		
-	
-		
+	public static void main(String[] args) {
+		Dice dice=new Dice();
+		dice.rollDice(3, 3);
 	}
+	public void rollDice(int attUnits,int defUnits) {
+		int lossDef=0;
+		int lossAtt=0;
+		ArrayList<Integer> aList= new ArrayList<Integer>();
+		ArrayList<Integer> defList= new ArrayList<Integer>();
+		for(int i=0;i<attUnits;i++) {
+			aList.add((int)(Math.random() *6)+1);
+			
+		}
+		for(int j=0;j<defUnits;j++) {
+			defList.add((int)(Math.random() *6)+1);
+			
+		}
+		System.out.println("d1" + " " + defList.get(0));
+		System.out.println("d2" + " " + defList.get(1));
+		System.out.println("d3" + " " + defList.get(2));
+		System.out.println("a1" + " " + aList.get(0));
+		System.out.println("a2" + " " + aList.get(1));
+		System.out.println("a3" + " " + aList.get(2));
+		System.out.println(" ");
+		Collections.sort(aList);
+		Collections.sort(defList);
+		Collections.reverse(aList);
+		Collections.reverse(defList);
+		System.out.println("d1" + " " + defList.get(0));
+		System.out.println("d2" + " " + defList.get(1));
+		System.out.println("d3" + " " + defList.get(2));
+		System.out.println("a1" + " " + aList.get(0));
+		System.out.println("a2" + " " + aList.get(1));
+		System.out.println("a3" + " " + aList.get(2));
+		if(aList.size()-defList.size()==2) {
+			aList.remove(2);
+			aList.remove(1);
+			
+		}
+		if(aList.size()-defList.size()==1) {
+			aList.remove(defList.size());
+			}
+		
+		if(defList.size()-aList.size()==2) {
+			defList.remove(2);
+			defList.remove(1);
+			
+		}
+		if(defList.size()-aList.size()==1) {
+			defList.remove(aList.size());
+			
+		}
+		
+		if(defList.size()==1) {
+			if(aList.get(0)>defList.get(0))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+		}
+		if(defList.size()==2) {
+			if(aList.get(0)>defList.get(0))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+			if(aList.get(1)>defList.get(1))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+		}
+		if(defList.size()==3) {
+			if(aList.get(0)>defList.get(0))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+			if(aList.get(1)>defList.get(1))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+			if(aList.get(2)>defList.get(2))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+		}
+		
+		System.out.println("Def:" + lossDef + " " + "Att" + lossAtt);
+		
 	
-}
-int defUnits = 1;
-int attUnits = 1;
-
+	}
 }
