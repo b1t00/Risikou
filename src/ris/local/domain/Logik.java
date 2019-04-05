316507b8a21c2aac9c3aca185658370da0ee2313
HannesLE <hannesl0123@gmail.com>
2019-04-05 07:47:26 +0200

    Update Logik.java

--- src/ris/local/domain/Logik.java
+++ src/ris/local/domain/Logik.java
@@ -1,5 +1,112 @@
 package ris.local.domain;
 
-public class Logik {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Logik {
+	
+	public ArrayList<Integer> verteileEinheiten(){}
+	
+	
+	public String attack(ausgang,ziel,menge) {}
+	
+	boolean angriffMöglich() {}
+	
+	public void angriffAuswerten(verteileEinheiten) {}
+	
+	public void moveUnits() {}
+	public boolean movePossible() {}
+	
+	public String landStatus() {}
+	public int unitsAvailable(Land) {}
+	public String showInBesitz(inBesitz) {}
+	
+	
+	
+	public ArrayList<Integer> rollDice(int attUnits,int defUnits) {
+		int lossDef=0;
+		int lossAtt=0;
+		ArrayList<Integer> aList= new ArrayList<Integer>();
+		ArrayList<Integer> defList= new ArrayList<Integer>();
+		for(int i=0;i<attUnits;i++) {
+			aList.add((int)(Math.random() *6)+1);
+			
+		}
+		for(int j=0;j<defUnits;j++) {
+			defList.add((int)(Math.random() *6)+1);
+			
+		}
+		System.out.println("d1" + " " + defList.get(0));
+		System.out.println("d2" + " " + defList.get(1));
+		System.out.println("d3" + " " + defList.get(2));
+		System.out.println("a1" + " " + aList.get(0));
+		System.out.println("a2" + " " + aList.get(1));
+		System.out.println("a3" + " " + aList.get(2));
+		System.out.println(" ");
+		Collections.sort(aList);
+		Collections.sort(defList);
+		Collections.reverse(aList);
+		Collections.reverse(defList);
+		System.out.println("d1" + " " + defList.get(0));
+		System.out.println("d2" + " " + defList.get(1));
+		System.out.println("d3" + " " + defList.get(2));
+		System.out.println("a1" + " " + aList.get(0));
+		System.out.println("a2" + " " + aList.get(1));
+		System.out.println("a3" + " " + aList.get(2));
+		if(aList.size()-defList.size()==2) {
+			aList.remove(2);
+			aList.remove(1);
+			
+		}
+		if(aList.size()-defList.size()==1) {
+			aList.remove(defList.size());
+			}
+		
+		if(defList.size()-aList.size()==2) {
+			defList.remove(2);
+			defList.remove(1);
+			
+		}
+		if(defList.size()-aList.size()==1) {
+			defList.remove(aList.size());
+			
+		}
+		
+		if(defList.size()==1) {
+			if(aList.get(0)>defList.get(0))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+		}
+		if(defList.size()==2) {
+			if(aList.get(0)>defList.get(0))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+			if(aList.get(1)>defList.get(1))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+		}
+		if(defList.size()==3) {
+			if(aList.get(0)>defList.get(0))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+			if(aList.get(1)>defList.get(1))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+			if(aList.get(2)>defList.get(2))
+				lossDef= lossDef-1;
+			else
+				lossAtt= lossAtt-1;
+		}
+		ArrayList<Integer> unitLoss= new ArrayList<Integer>();
+		unitLoss.add(lossDef);
+		unitLoss.add(lossAtt);
+		System.out.println("Def:" + lossDef + " " + "Att" + lossAtt);
+		return unitLoss;
+	
+	}
 }
