17232ea7933e7ddfbb46c752b0c80f6aeca188d3
AnnCharBaer <48607023+AnnCharBaer@users.noreply.github.com>
2019-04-05 17:20:15 +0200

    changes

--- src/ris/local/domain/Risiko.java
+++ src/ris/local/domain/Risiko.java
@@ -1,5 +1,6 @@
 package ris.local.domain;
 
+import ris.local.valueobjects.Land;
 import ris.local.valueobjects.Player;
 
 public class Risiko {
@@ -32,6 +33,7 @@ public class Risiko {
 	
 	public Player gibAktivenSpieler() {
 		return logik.gibAktivenSpieler();
+		//@annie: hier muss der name vom aktiven Spieler zurückgegeben werden
 	}
 	
 	public void naechsterSpieler() {
@@ -39,3 +41,17 @@ public class Risiko {
 	}
 
 }
+
+/* @annie: irgendwo array mit farben, auf die zugegriffen werden kann mit methode gibAlleFarben()
+ * bei erstellung des Players muss dann die übergebene Farbe aus dem array herausgenommen werden */
+// @annie: die Verteilung der Einheiten direkt nach dem Erstellen der Spieler wird mit risiko.verteileEinheiten() aufgerufen
+public String gibLaenderAus(Player player) {
+	return player.gibLaenderAus();
+} // in der methode vielleicht name und besitzt herausnehmen
+public void setztEinheit(int land, int einheit) {
+	//int land in Land land umwandeln und dann setEinheit()
+}
+// @annie: getAnzahlPlayer
+//@annie: methode getFeinde(Player angreifer, int land)
+//@annie: methode getEinheiten(int land)
+//@annie: methode getBesitzer(int land)
--- src/ris/local/ui/cui/RisikoClientUI.java
+++ src/ris/local/ui/cui/RisikoClientUI.java
@@ -16,24 +16,23 @@ import ris.local.valueobjects.Land;
 public class RisikoClientUI {
 
 	private Risiko risiko;
-
 	private BufferedReader in;
-	private String name, farbe;
-	private Player spieler;
-	private Collection<String> farbenAuswahl = new HashSet();
-
-	// variablen für Spielvergabe (WIRD NOCH NICHT BENUTZT)
-	int anzahlAnSpielern;
 
 	// Konstruktor
 	private RisikoClientUI() {
-
 		risiko = new Risiko();
 		in = new BufferedReader(new InputStreamReader(System.in));
-		farbenAuswahl.add("rot");
-		farbenAuswahl.add("gruen");
-		farbenAuswahl.add("blau");
-
+	}
+	
+	public static void main(String[] args) {
+		RisikoClientUI cui = new RisikoClientUI();
+		cui.run();
+	}
+	
+	public void run() {		
+		starteSpiel();
+		verteileStartEinheiten();
+		round();
 	}
 
 	// einlesen von Konsole
@@ -41,43 +40,36 @@ public class RisikoClientUI {
 		return in.readLine();
 	}
 
-	private void anfangsMenue() {
+	private void starteSpiel() {
 		boolean richtigeEingabe = false;
 		String eingabe = "";
 		while (!richtigeEingabe) {
 			System.out.println("Lust Risiko zu spielen? (y/n) :	");
 			try {
 				eingabe = liesEingabe();
-			} catch (IOException e) {
-			}
+			} catch (IOException e) {}
 			switch (eingabe) {
-			case "yes":
-			case "j":
-			case "y":
+			case "yes": case "j": case "y":
 				try {
 					wieVieleSpielerMenu();
-				} catch (IOException e) {
-				}
+				} catch (IOException e) {}
 				System.out.println("jetzt beginnt das Spiel"); // Platzhalter für Spielanfang
 				richtigeEingabe = true;
 				break;
-			case "no":
-			case "nö":
-			case "n":
+			case "no": case "nö": case "n":
 				System.out.println("Risik wird beendet"); // Platzhalter für Spielbeenden
-
 				richtigeEingabe = true;
 				break;
 			default:
 				System.out.println("ungültige eingabe");
 				richtigeEingabe = false;
-
 			}
 		}
+		//zufällige Verteilung der Einheiten
+		risiko.verteileEinheiten();
 	}
 
 	public void wieVieleSpielerMenu() throws IOException {
-
 		String eingabeSpieler, name, farbe;
 		int nr;
 		System.out.println("Wieviele Spieler soll es geben? :");
@@ -93,6 +85,7 @@ public class RisikoClientUI {
 
 	public String farbeAuswaehlen() {
 		String farbe = "";
+		ArrayList<String> farbenAuswahl = risiko.gibAlleFarben();
 
 		System.out.println("Welche Farbe möchtest nehmen?");
 		if (farbenAuswahl.contains("rot")) {
@@ -106,19 +99,15 @@ public class RisikoClientUI {
 		}
 		try {
 			farbe = liesEingabe();
-		} catch (IOException e) {
-		}
+		} catch (IOException e) {}
 		switch (farbe) {
 		case "r":
 			farbe = "rot";
-			farbenAuswahl.remove("rot");
 			break;
 		case "g":
-			farbenAuswahl.remove("gruen");
 			farbe = "gruen";
 			break;
 		case "b":
-			farbenAuswahl.remove("blau");
 			farbe = "blau";
 			break;
 		default:
@@ -126,10 +115,92 @@ public class RisikoClientUI {
 		}
 		return farbe;
 	}
-
-	public static void main(String[] args) {
-		RisikoClientUI cui = new RisikoClientUI();
-		cui.anfangsMenue();
+	
+	public void verteileStartEinheiten() {
+		int anzahlEinheiten = risiko.getAnzahlPlayer() * 5;
+		int einheit = 1;
+		
+		while(anzahlEinheiten > 0) {
+			Player aktiverPlayer = risiko.gibAktivenSpieler();
+			System.out.println(aktiverPlayer + ": setze eine Einheit.");
+			System.out.println(risiko.gibLaenderAus(aktiverPlayer));
+			int land;
+			try{
+				//!!! hier muss noch geprüft werden, ob die zahl überhaupt zur auswahl stand > evtl. arraylist land geben lassen und dann mit equals
+				land = Integer.parseInt(liesEingabe());
+			} catch(IOException e) {}
+			risiko.setztEinheit(int land, int einheit);
+			anzahlEinheiten--;
+			risiko.naechsterSpieler();
+		}
 	}
+	
+	public void round() {
+		Player aktiverPlayer = risiko.gibAktivenSpieler();
+		String input = "";
+		while(true) {
+			//spieler bekommt einheiten
+			gibMenuAus(aktiverPlayer);
+			try {
+				input = liesEingabe();
+				verarbeiteEingabe(input, aktiverPlayer);
+			} catch(IOException e){}
+
+			risiko.naechsterSpieler();
+		}
+	}
+	
+	public void gibMenuAus(Player aktiverPlayer) {
+		System.out.print(aktiverPlayer + ": Was möchtest du tun?");
+		System.out.print("               \n Angreifen: a");
+		System.out.print("               \n Einheiten verschieben: e");
+		System.out.print("               \n Zug beenden: z");
+		System.out.print("               \n Spiel beenden: q");
+		System.out.flush();
+	}
+	
+	public void verarbeiteEingabe(String input, Player aktiverPlayer) {
+			switch(input) {
+			case "a":
+				attack(aktiverPlayer);
+				break;
+			case "e":
+				//
+				break;
+			case "z":
+				//
+				break;
+			case "q":
+				//
+				break;
+			default:
+				//
+				break;
+			}
+	}
+	
+	public void attack(Player angreifer) {
+		int start;
+		int ziel;
+		int angriff;
+		int verteidigung;
+		System.out.print(angreifer + " mit welchem Land möchtest du angreifen?");
+		System.out.println(risiko.gibLaenderAus(angreifer));
+		try {
+			start = Integer.parseInt(liesEingabe()); 
+		} catch(IOException e) {}
+		System.out.println("Welches Land soll angegriffen werden? \n" + risiko.getFeinde(angreifer, start));
+		try {
+			ziel = Integer.parseInt(liesEingabe());
+		} catch(IOException e) {}
+		System.out.println("Mit wie vielen Einheiten soll angegriffen werden? Maximal:" + risiko.getEinheiten(start));
+		try {
+			angriff = Integer.parseInt(liesEingabe());
+		} catch(IOException e) {}
+		Player defender = risiko.getBesitzer(ziel);
+		System.out.println(defender + ": Mit wievielen Einheiten möchtest du verteidigen? Maximal: " + risiko.getEinheiten(ziel));
+		risiko.attack (start)
+	}
+
 
 }
