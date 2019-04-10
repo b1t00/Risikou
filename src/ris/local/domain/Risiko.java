bcfe554ec0bfb8dc03dd85af63d02d225de20298
b1t00 <48808048+b1t00@users.noreply.github.com>
2019-04-10 19:59:39 +0200

    Update Risiko.java

--- src/ris/local/domain/Risiko.java
+++ src/ris/local/domain/Risiko.java
@@ -71,31 +71,5 @@ public class Risiko {
 	
 	}
 
-	public static void main(String[] args) {
-
-		Risiko rT = new Risiko();
-		rT.spielerAnlegen("Anna", "rot", 1);
-		rT.spielerAnlegen("Leo", "blau", 2);
-		rT.spielerAnlegen("Normi", "gruen", 3);
-		rT.spielerAnlegen("berta", "gruen", 3);
-		rT.spielerAnlegen("Normi", "gruen", 3);
-		rT.spielerAnlegen("Normi3", "gruen", 3);
-
-		
-//		int p = rT.logik.laenderAnzahl;
-//		System.out.println("es gibt insgesammt " + p);
-		
-//		rT.logik.shuffleLaender();
-		rT.verteileEinheiten();
-		List<Player> spielerListe = rT.playerMg.getPlayers();
-		for (int i = 0; i < rT.playerMg.getPlayerAnzahl(); i++) {
-			System.out.println(spielerListe.get(i).gibLaenderAus());
-			System.out.println(spielerListe.get(i).getBesitz().size());
-			System.out.println("-----");
-		}
-		rT.whoBegins(); // kann man in verteileEinheiten tun
-		rT.gibLaenderAus();
-
-	}
 
 }
