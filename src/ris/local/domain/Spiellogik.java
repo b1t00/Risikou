8f8dcbe2f9d19b4ce91fbd1d3ae399ade8f851b1
AnnCharBaer <48607023+AnnCharBaer@users.noreply.github.com>
2019-05-08 12:45:25 +0200

    Update Spiellogik.java

--- src/ris/local/domain/Spiellogik.java
+++ src/ris/local/domain/Spiellogik.java
@@ -112,10 +112,17 @@ public class Spiellogik implements Serializable {
 	// ----------------------------------------einheiten-------------------------------------------------
 	public boolean changePossible(Player aktiverPlayer) {
 		int[] symbolAnzahlArray = aktiverPlayer.risikokartenKombi();
+		boolean reihe = true;
 		for (int i = 1; i < symbolAnzahlArray.length; i++) {
 			if (symbolAnzahlArray[i] > 2) {
 				return true;
 			}
+			//Eine Reihe wird überprüft, in dem ab dem Moment, wenn ein Symbol auf 0 ist, der boolean reihe auf false gesetzt wird
+			else if (symbolAnzahlArray[i]==0) {
+				reihe = false;
+			}
+		} if(reihe) {
+			return true;
 		}
 		return false;
 	}
