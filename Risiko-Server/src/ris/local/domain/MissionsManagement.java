bfb91eb1c6b59b8e1730ababd71e690183d9f963
AnnCharBaer <48607023+AnnCharBaer@users.noreply.github.com>
2019-07-05 11:31:51 +0200

    changes

--- Risiko-Server/src/ris/local/domain/MissionsManagement.java
+++ Risiko-Server/src/ris/local/domain/MissionsManagement.java
@@ -22,14 +22,14 @@ public class MissionsManagement implements Serializable {
 		this.worldMg = worldMg;
 		missionen = new ArrayList<Mission>();
 
-//		missionen.add(new MissionKontinent(worldMg.getKontinente().get(0), worldMg.getKontinente().get(1)));
-//		missionen.add(new MissionKontinent(worldMg.getKontinente().get(2), worldMg.getKontinente().get(4)));
-//		missionen.add(new MissionKontinent(worldMg.getKontinente().get(3), worldMg.getKontinente().get(5)));
+		missionen.add(new MissionKontinent(worldMg.getKontinente().get(0), worldMg.getKontinente().get(1)));
+		missionen.add(new MissionKontinent(worldMg.getKontinente().get(2), worldMg.getKontinente().get(4)));
+		missionen.add(new MissionKontinent(worldMg.getKontinente().get(3), worldMg.getKontinente().get(5)));
 		missionen.add(new MissionLaenderanzahl());
 		missionen.add(new MissionLaenderanzahl());
-//		for (Player player : playerMg.getPlayers()) {
-//			missionen.add(new MissionGegner(player));
-//		}
+		for (Player player : playerMg.getPlayers()) {
+			missionen.add(new MissionGegner(player));
+		}
 	}
 
 	public ArrayList<Mission> getMission() {
--- Risikou-Common/src/ris/common/valueobjects/MissionLaenderanzahl.java
+++ Risikou-Common/src/ris/common/valueobjects/MissionLaenderanzahl.java
@@ -6,12 +6,13 @@ public class MissionLaenderanzahl extends Mission implements Serializable {
 
 	public MissionLaenderanzahl() {
 		super("mission2 : Erobere 24 Laender deiner Wahl");
+//		super("mission2 : Erobere 22 Laender deiner Wahl");
 	}
 
 	@Override
 	public boolean missionComplete(Player aktiverSpieler) {
-//		if (aktiverSpieler.getBesitz().size() > 23) {
-		if (aktiverSpieler.getBesitz().size() > 21) {
+		if (aktiverSpieler.getBesitz().size() > 23) {
+//		if (aktiverSpieler.getBesitz().size() > 21) {
 			return true;
 		}
 		return false;
